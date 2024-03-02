c6b37eb5cb1aa9578ef2e17ad62fdce5189d3d6b
Ekaterina Bobrova <merichiko@yandex.ru>
2024-03-02 10:58:41 +0300

    create & delete card element

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,39 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+function createCard(cardData, deleteCallback) {
+    const cardTemplate = document.getElementById('card-template');
+    const cardElement = cardTemplate.content.firstElementChild.cloneNode(true);
+    
+    const imageElement = cardElement.querySelector('.card__image');
+    imageElement.src = cardData.link;
+    imageElement.alt = cardData.name;
+    
+    const titleElement = cardElement.querySelector('.card__title');
+    titleElement.textContent = cardData.name;
+    
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {
+    deleteCallback(cardData);
+    cardElement.remove();
+    });
+    
+    return cardElement;
+}
+
+const placesList = document.querySelector('.places__list');
+
+const cardElements = initialCards.map(card => createCard(card, (cardToDelete) => {
+
+    const index = initialCards.findIndex(item => item === cardToDelete);
+    if (index !== -1) {
+    initialCards.splice(index, 1);
+    }
+}));
+
+
+cardElements.forEach(cardElement => {
+    placesList.appendChild(cardElement);
+});
